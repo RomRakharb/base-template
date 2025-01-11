9281777ce9e87ff7241b2874f6a20b73f8c66b82
Rom Rakharb <romr.dev@proton.me>
2025-01-11 03:52:15 +0000

    edit install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/bash
 
+echo
 echo "starts install script"
+echo
 sudo apt update && sudo apt -y upgrade
 sudo apt-get install -y software-properties-common
 sudo apt update
 sudo add-apt-repository -y ppa:maveonair/helix-editor
 sudo apt update
 sudo apt install helix
+echo
 echo "install script ends"
+echo
