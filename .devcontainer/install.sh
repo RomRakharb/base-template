fb4856e551a889df8ad212df33d9d9526a5a2698
Rom Rakharb <121065878+RomRakharb@users.noreply.github.com>
2025-04-08 11:32:24 +0700

    Update install.sh

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,14 +5,26 @@ echo "---------------------"
 echo "starts install script"
 echo "---------------------"
 echo
+
 sudo apt update && sudo apt -y upgrade
-sudo apt-get install -y software-properties-common
-sudo apt update
-sudo add-apt-repository -y ppa:maveonair/helix-editor
-sudo apt update
-sudo apt install helix
-cargo install bacon
+npm install npm@latest -g
+
+git clone https://github.com/helix-editor/helix.git
+cargo install --path ./helix/helix-term --locked
+hx --grammar fetch
+hx --grammar build
+cp -r ./helix/runtime/queries ~/.config/helix/runtime/
+cp -r ./helix/runtime/themes ~/.config/helix/runtime/
+cp ./helix/runtime/tutor ~/.config/helix/runtime/
+echo -e "[editor]\ntrue-color = true" > ~/.config/helix/config.toml
+rm -f helix -R
+
+cargo install bacon --locked
+
 rustup component add rust-analyzer
+npm i -g vscode-langservers-extracted
+rustup target add wasm32-unknown-unknown
+
 echo
 echo "---------------------"
 echo "install script ends"
